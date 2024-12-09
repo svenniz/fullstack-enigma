af0e7f45e8417fb3389ea508f292610d72bc2b7a
Andreas Ventzel <a-spac01@specialisterne.dk>
2024-12-09 14:23:16 +0100

    mappingprofile: added map for product and dto

--- EnigmaApi/EnigmaApi/Services/MappingProfile.cs
+++ EnigmaApi/EnigmaApi/Services/MappingProfile.cs
@@ -1,8 +1,14 @@
 ﻿using AutoMapper;
+using EnigmaApi.Dtos;
+using EnigmaApi.Models;
 
 namespace EnigmaApi.Services
 {
     public class MappingProfile : Profile
     {
+        public MappingProfile() 
+        {
+            CreateMap<Product, ProductDto>().ReverseMap();
+        }
     }
 }
