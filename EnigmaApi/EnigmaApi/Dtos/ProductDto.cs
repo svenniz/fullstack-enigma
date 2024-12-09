b50db4ddf08ec6c8079fc97a1702dea67406f342
Andreas Ventzel <a-spac01@specialisterne.dk>
2024-12-09 13:32:22 +0100

    added product and dto

---
+++ EnigmaApi/EnigmaApi/Dtos/ProductDto.cs
@@ -0,0 +1,9 @@
+﻿namespace EnigmaApi.Dtos
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal? Price { get; set; }
+    }
+}
---
+++ EnigmaApi/EnigmaApi/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace EnigmaApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal? Price { get; set; }
+        public string? Secret { get; set; }
+    }
+}
