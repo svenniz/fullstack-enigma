69f5a30c9a89e5c564e37f3d207a1e7a9945ead4
Andreas Ventzel <a-spac01@specialisterne.dk>
2025-01-17 10:06:43 +0100

    commented out player and playerid in deck.cs

--- EnigmaApi/EnigmaApi/Models/Deck.cs
+++ EnigmaApi/EnigmaApi/Models/Deck.cs
@@ -5,9 +5,8 @@
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
-        public int? PlayerId { get; set; }
-        public Player? Player { get; set; } // Navigation property
-        // Navigation property
-        public ICollection<DeckCard> DeckCards { get; set; } = new List<DeckCard>();
+        //public int? PlayerId { get; set; }
+        //public Player? Player { get; set; } // Navigation property
+        public ICollection<DeckCard> DeckCards { get; set; } = new List<DeckCard>(); // Navigation property
     }
 }
